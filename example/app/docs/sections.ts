8897e243fda703e7fe637a2467484583b64e1c1b
Austin Malerba <austin.malerba@gmail.com>
2025-01-19 18:28:02 -0700

    stuff

--- example/app/docs/sections.ts
+++ example/app/docs/sections.ts
@@ -1,6 +1,7 @@
 import IntroMdx from "./intro.mdx";
 import GettingStartedMdx from "./getting-started.mdx";
 import StoreMdx from "./store.mdx";
+import RelationshipsMdx from "./relationships.mdx";
 
 export const sections = [
   {
@@ -17,5 +18,10 @@ export const sections = [
     id: "store",
     label: "Store",
     Markdown: StoreMdx,
+  },
+  {
+    id: "relationships",
+    label: "Relationships",
+    Markdown: RelationshipsMdx,
   },
 ];
